5f2844dafdfafd4309ff9cf8a9df93b5f6f6c208
Shiro Miminashi Nohara <s.nohara.2001@gmail.com>
2016-01-19 00:09:34 +0900

    disables if pin is unused

--- main.py
+++ main.py
@@ -5,8 +5,7 @@ import mraa
 import time
 import math
 
-#LED_PIN_NUMBERS = [3, 5, 6, 9]
-LED_PIN_NUMBERS = [3, 5, 6]
+LED_PIN_NUMBERS = [3, 5, 6, 9]
 MIN_VALUE  = 0.0
 MAX_VALUE  = 1.0
 PWM_INTERVAL = 10000  # 10msec
@@ -15,13 +14,14 @@ def map_exp_0_to_255(value):
     v = value * 8.0 / 255.0
     return math.pow(2.0, v) - 1.0
 
-pins = []
+pins = {}
 
 for pin_number in LED_PIN_NUMBERS:
     pin = mraa.Pwm(pin_number)
     pin.period_us(PWM_INTERVAL)
-    pin.enable(True)
-    pins.append(pin)
+    #pin.enable(True)
+    #pin.write(0)
+    pins[pin_number] = pin
 
 
 value = 0
@@ -52,7 +52,15 @@ direction = 1
 #    time.sleep(0.99)
 
 # light on
+on_pin_numbers = [3, 5]
+for pin_number in LED_PIN_NUMBERS:
+    pin = pins[pin_number]
+    pin.enable(False)
+for pin_number in on_pin_numbers:
+    pin = pins[pin_number]
+    pin.enable(True)
+    pin.write(1)
+
+
 while True:
-    for pin in pins:
-        pin.write(1)
-    time.sleep(0.1)
+    pass
